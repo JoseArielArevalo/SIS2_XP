89725131ba8a7d621fc178f745557aa2949c66d5
irvin373 <irvin373@gmail.com>
2015-09-09 14:37:57 +0200

    TestVerificarDestino

--- SIS2_XP/test/TestUsersDestiny.java
+++ SIS2_XP/test/TestUsersDestiny.java
@@ -9,6 +9,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import java.util.*;
 import static org.junit.Assert.*;
 
 /**
@@ -30,10 +31,37 @@ public class TestUsersDestiny {
     
     @Before
     public void setUp() {
+        List<String> rutaBus = new ArrayList<String>();
+        rutaBus.add("Laguna");
+        rutaBus.add("UMSS");
+        rutaBus.add("Universidad Catolica");
+        rutaBus.add("America");
+        rutaBus.add("IC. Norte");
+        rutaBus.add("America y Libertadores");
+        rutaBus.add("America y Melchor Perez");
+        rutaBus.add("IC. Norte");
+        rutaBus.add("Blanco Galindo");
+        Destino destino;
     }
     
     @After
     public void tearDown() {
+    }
+    
+    @Test
+    public void TestVerificarDestino(){
+        int idCliente = 805038;
+        destino = new Destino(idCliente,"Laguna","IC. Norte");
+        assertEquals(destino.Verificar(), "Destino Valido");
+        
+        destino = new Destino(idCliente,"IC. Norte","Laguna");
+        assertEquals(destino.Verificar(), "Destino ya recorrido");
+        
+        destino = new Destino(idCliente,"IC. Norte","Circunvalacion");
+        assertEquals(destino.Verificar(), "Destino no encuentrado");
+        
+        destino = new Destino(idCliente,"IC. Norte","IC. Norte");
+        assertEquals(destino.Verificar(), "Ya se encuentra en el destino");
     }
 
     // TODO add test methods here.
